0fa361720bfcc3c4a2191e6f783aa1bd48f9e0a6
ExPress <belkinnik93@gmail.com>
2022-12-25 20:20:03 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30_000;
+        int bonus = 20;
+
+
+        int result = ticketPrice / bonus;
+
+        System.out.println(result);
+
+
+    }
+}
